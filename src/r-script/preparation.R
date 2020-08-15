dc3fe1d7c9e7d2e37310680fd2ac5a282db26857
LukasBaecker <65940705+LukasBaecker@users.noreply.github.com>
2020-08-15 16:24:52 +0200

    Update preparation.R

--- src/r-script/preparation.R
+++ src/r-script/preparation.R
@@ -15,11 +15,13 @@ setwd('C:/Users/__REPOSITORY_DIR__')
 #stack JPEG2000
 sentinel_west <- stack('__PATH_TO__/T32ULC_20200427T104031_B02.jp2',
                       '__PATH_TO__/T32ULC_20200427T104031_B03.jp2',
-                      '__PATH_TO__/T32ULC_20200427T104031_B04.jp2')
+                      '__PATH_TO__/T32ULC_20200427T104031_B04.jp2',
+                      '__PATH_TO__/T32ULC_20200427T104031_B08.jp2')
 
 sentinel_east <- stack('__PATH_TO__/T32UMC_20200427T104031_B02.jp2',
                       '__PATH_TO__/T32UMC_20200427T104031_B03.jp2',
-                      '__PATH_TO__/T32UMC_20200427T104031_B04.jp2') 
+                      '__PATH_TO__/T32UMC_20200427T104031_B04.jp2',
+                      '__PATH_TO__/T32UMC_20200427T104031_B08.jp2') 
 
 # crop to the extent of the study area
 west_crop <- crop(sentinel_west,c(390309.5, 418741.3, 5746125.4,5768919.6))
